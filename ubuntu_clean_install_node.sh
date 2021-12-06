c80f3c705420795853a6c194005b559b827ca2b8
ranafire <32353518+ranaFire@users.noreply.github.com>
2021-12-06 12:39:17 +0200

    Create ubuntu_clean_install_node.sh

---
+++ ubuntu_clean_install_node.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get purge nodejs npm
+sudo apt-get remove nodejs npm node
+sudo apt-get purge nodejs
+sudo rm -rf /usr/local/bin/npm 
+sudo rm -rf /usr/local/share/man/man1/node* 
+sudo rm -rf /usr/local/lib/dtrace/node.d
+rm -rf ~/.npm
+rm -rf ~/.node-gyp
+sudo rm -rf /opt/local/bin/node
+sudo rm -rf /opt/local/include/node
+sudo rm -rf /opt/local/lib/node_modules
+sudo rm -rf /usr/local/lib/node*
+sudo rm -rf /usr/local/include/node*
+sudo rm -rf /usr/local/bin/node*
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install -y nodejs
