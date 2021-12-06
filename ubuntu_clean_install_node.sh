24f33069f7fde97037e88aa742faeff4590ec7b0
ranafire <32353518+ranaFire@users.noreply.github.com>
2021-12-06 12:46:22 +0200

    Update ubuntu_clean_install_node.sh

--- ubuntu_clean_install_node.sh
+++ ubuntu_clean_install_node.sh
@@ -16,3 +16,4 @@ sudo rm -rf /usr/local/include/node*
 sudo rm -rf /usr/local/bin/node*
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 sudo apt-get install -y nodejs
+# sudo curl https://raw.githubusercontent.com/ranaFire/script/master/ubuntu_clean_install_node.sh | bash
