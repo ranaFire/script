0cabd54421551144e988a45ff500bd3030576e70
ranafire <32353518+ranaFire@users.noreply.github.com>
2022-01-14 03:07:55 +0200

    Create ubuntu_install_multipal_java.sh

---
+++ ubuntu_install_multipal_java.sh
@@ -0,0 +1,6 @@
+sudo apt install openjdk-8-jdk openjdk-8-jre -y
+sudo apt install openjdk-11-jdk openjdk-11-jre -y
+sudo apt install openjdk-17-jdk openjdk-17-jre -y
+
+#Select the java version
+sudo update-alternatives --config java
